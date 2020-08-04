98325ba8fcd58c50a0f13c5c6fca0899c2e83bb9
justgoodin <rehan.haider.v3@gmail.com>
2020-08-04 12:35:46 +0530

    Fixed redefinition of inbuilt method

--- 01.HackerRank/01.Practice/01.ProblemSolving/01.Algorithm.05.DiagonalDifference.py
+++ 01.HackerRank/01.Practice/01.ProblemSolving/01.Algorithm.05.DiagonalDifference.py
@@ -14,14 +14,14 @@ import sys
 #
 
 def diagonalDifference(arr):
-    sum = 0
+    sumOfArr = 0
     size = len(arr)-1
     i = 0
     while i<=size:
-        sum = sum + (arr[i][i]-arr[size-i][i])
+        sumOfArr = sumOfArr + (arr[i][i]-arr[size-i][i])
         i += 1
     
-    return abs(sum)
+    return abs(sumOfArr)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
