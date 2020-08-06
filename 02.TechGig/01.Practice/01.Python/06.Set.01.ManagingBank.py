2d43d69113d9480b4ff776a9846cbb46b2426f47
justgoodin <rehan.haider.v3@gmail.com>
2020-08-06 20:11:52 +0530

    Create 06.Set.01.ManagingBank.py

---
+++ 02.TechGig/01.Practice/01.Python/06.Set.01.ManagingBank.py
@@ -0,0 +1,10 @@
+#Link to the question below
+#https://www.techgig.com/practice/question/managing-bank/UlhnRDdwQittTXVvMms1UHMyWnB5UVo0T2pWRHUyRmtqQjl6aU5vcWR3TEJqWkZzZm9uQklsdFQ1R3FhSWpDbg==/1
+
+def main():
+    N = int(input())
+    AC = set(list(map(int,input().split())))
+    print(len(AC),end="")
+
+main()
+
