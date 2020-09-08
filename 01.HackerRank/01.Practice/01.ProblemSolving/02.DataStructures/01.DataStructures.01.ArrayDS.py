f43fa185cd97567748af4e864af11698af4ffa00
justgoodin <=>
2020-09-09 01:18:42 +0530

    NA

---
+++ 01.HackerRank/01.Practice/01.ProblemSolving/02.DataStructures/01.DataStructures.01.ArrayDS.py
@@ -0,0 +1,24 @@
+#Link to the question below
+#https://www.hackerrank.com/challenges/arrays-ds/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def reverseArray(a):
+    return a[::-1]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    arr_count = int(input())
+    arr = list(map(int, input().rstrip().split()))
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+    fptr.close()
