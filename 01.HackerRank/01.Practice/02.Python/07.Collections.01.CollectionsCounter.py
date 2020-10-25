8f01535d3f7230bb2c58f66b841c899bc73ca793
justgoodin <rehan.haider.v3@gmail.com>
2020-10-25 15:51:53 +0530

    NA

---
+++ 01.HackerRank/01.Practice/02.Python/07.Collections.01.CollectionsCounter.py
@@ -0,0 +1,2 @@
+#Link to the question below
+#https://www.hackerrank.com/challenges/collections-counter/problem
---
+++ 01.HackerRank/01.Practice/03.C/01.Introduction.04.PointersInC.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void update(int *a,int *b) {
+    int c = *a + *b;
+    *b = abs(*a-*b);
+    *a = c;
+
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
